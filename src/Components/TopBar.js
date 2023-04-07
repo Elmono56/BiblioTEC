1bff6d8321113d55a2029032edcff642859aa2df
Axel Chaves <axelchaves.r@estudiantec.cr>
2023-04-07 15:43:39 -0600

    La imagen del TEC ahora regresar al login. Además, se cambió el título del proyecto

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LogoTec from "../img/logoTEC.png";
+import { useNavigate } from 'react-router-dom';
 const TopBar = () => {
+	const navigate = useNavigate();
 	return (
 		<div id="top-bar">
 			<div id="logo">
-				<img src={LogoTec} alt="logo" id="logoTEC" />
+				<img src={LogoTec} alt="logo" id="logoTEC" onClick={() => navigate('/')}/>
 				<h1 id="appTitle">BiblioTEC</h1>
 				<h2 id="appDescription">Sistema Gestor de Cubículos</h2>
 			</div>
