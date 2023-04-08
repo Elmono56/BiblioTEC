3266e3fa3aee9edbb818d12a0f7feab52d26eede
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-04-07 23:57:21 -0600

    fix ortografia_v2

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -13,9 +13,9 @@ const Menu = () => {
 		<>
 			<div id="menu-btns">
 				<h1 className='page-title'>Menu Estudiantes</h1><br></br>
-				<button onClick={() => navigateReservar('/menu/reservarCubiculo')} className='est-chooseOption'>Reservar cubiculo</button><br></br>
-				<button onClick={() => navigateCancelar('/menu/cancelarReserva')} className='est-chooseOption'>Cancelar reserva de cubiculo</button><br></br>
-				<button onClick={() => navigateVerCubiculos('/menu/verCubiculos')} className='est-chooseOption'>Ver cubiculos disponibles</button><br></br>
+				<button onClick={() => navigateReservar('/menu/reservarCubiculo')} className='est-chooseOption'>Reservar cubículo</button><br></br>
+				<button onClick={() => navigateCancelar('/menu/cancelarReserva')} className='est-chooseOption'>Cancelar reserva de cubículo</button><br></br>
+				<button onClick={() => navigateVerCubiculos('/menu/verCubiculos')} className='est-chooseOption'>Ver cubículos disponibles</button><br></br>
 				<button onClick={() => navigateVerHistorial('/menu/verHistorial')} className='est-chooseOption'>Ver historial de reservas</button><br></br><br></br>
 				<button onClick={() => navigate('../')} className='est-chooseOption'>Salir</button><br></br>
 			</div>
