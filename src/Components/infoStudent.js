b457f6f05ec4aea551ea96ff721866a33b009b9f
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-04-09 13:57:18 -0600

    add some features

--- src/Components/infoStudent.js
+++ src/Components/infoStudent.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import TopBar from "./TopBar";
 import '../menu.css';
@@ -8,10 +8,15 @@ const InfoStudent = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [id, setId] = useState("");
+    const [status, setStatus] = useState("Activo");
 
     const navigate = useNavigate();
     const navigateInfo = useNavigate();
-    
+
+    const handleStatusChange = (event) => {
+        setStatus(event.target.value);
+    };
+
     return (
         <>
             <TopBar />
@@ -19,30 +24,87 @@ const InfoStudent = () => {
                 <h1 className="page-title">Información del estudiante</h1>
                 <form id="form-agregar">
                     <label>
-                        Nombre: <br></br>
-                        <input type="text" className="loginBoxes" name="name" value={name} 
-                            defaultValue={name}
-                            onChange={(e) => setName(e.target.value)}/><br></br>
+                        Nombre: <br />
+                        <input
+                            type="text"
+                            className="loginBoxes"
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <br />
                     </label>
                     <label htmlFor="username">
-                        Correo electrónico: <br></br>
-                        <input type="text" id="username" name="username" className="loginBoxes" value={username} 
-                            defaultValue={username}
-                            onChange={(e) => setUsername(e.target.value)}/>
+                        Correo electrónico: <br />
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="loginBoxes"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </label>
                     <label htmlFor="password">
-                        Contraseña: <br></br>
-                        <input type="password" id="password" className="loginBoxes" value={password} 
-                            defaultValue={username}
-                            onChange={(e) => setPassword(e.target.value)}/><br></br>
+                        Contraseña: <br />
+                        <input
+                            type="password"
+                            id="password"
+                            className="loginBoxes"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br />
                     </label>
                     <label>
                         Servicio especial:
-                        <input type="checkbox" name="cubAccs" value={id} 
+                        <input
+                            type="checkbox"
+                            name="cubAccs"
+                            value={id}
                             defaultValue={id}
-                            onChange={(e) => setId(e.target.value)}/>
-                        <br></br><br></br>
+                            onChange={(e) => setId(e.target.value)}
+                        />
+                        <br />
+                        <br />
                     </label>
+                    <div className="radio-container">
+                        <label >
+                            Estado de Estudiante: <br/>
+                            <input
+                                type="radio"
+                                id="activo"
+                                name="estado"
+                                value="Activo"
+                                className="radio-input"
+                                checked={status === 'Activo'}
+                                onChange={handleStatusChange}
+                            />
+                            <label>Activo</label>
+                            <br />
+                            <input
+                                type="radio"
+                                id="inactivo"
+                                name="estado"
+                                value="Inactivo"
+                                className="radio-input"
+                                checked={status === 'Inactivo'}
+                                onChange={handleStatusChange}
+                            />
+                            <label>Inactivo</label>
+                            <br />
+                            <input
+                                type="radio"
+                                id="moroso"
+                                name="estado"
+                                value="Moroso"
+                                className="radio-input"
+                                checked={status === 'Moroso'}
+                                onChange={handleStatusChange}
+                            />
+                            <label>Moroso</label>
+                        </label>
+                    </div>
                 </form>
                 <button onClick={() => navigateInfo('/menuAdmin/modificarEstudiante')} className='est-chooseOption'>
                     Aceptar
