1c1e1b20869209196b85ae3c54026e54b10e116e
Javier Vasquez <javiervasquez212@gmail.com>
2023-04-08 19:18:28 -0600

    ConexionBD Reporte/Historial

--- src/Components/ReportAdmin.js
+++ src/Components/ReportAdmin.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import TopBar from './TopBar';
 import '../menu.css';
 
 const ReportAdmin = () => {
-	const navigate = useNavigate();
-	return (
-		<>
-			<h1 className="page-title" style={{marginLeft: 480}}>Informe de uso del cubículo</h1>
-			<div id="menu-btns">
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
 
-				{/*<div id="report-content">
-					{/* Agrega aquí el contenido del informe 
-				</div>*/}
-				<br></br><br></br>
-				<button onClick={() => navigate('/menuAdmin')} className='est-chooseOption'>
-					Regresar
-				</button>
-			</div>
-			<TopBar />
-		</>
-	);
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get('http://localhost:3001/api/reporte');
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h1 className="page-title" style={{ marginLeft: 480 }}>
+        Informe de uso del cubículo
+      </h1>
+      <div id="menu-btns">
+        <div id="report-content">
+          {/* Agrega aquí el contenido del informe */}
+          <table>
+            <thead>
+              <tr>
+                <th>Número cubículo</th>
+                <th>Carnet</th>
+                <th>Fecha</th>
+				<th>Hora de inicio</th>
+				<th>Hora de final</th>
+				<th>Número de reserva</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.carnet}>
+                  <td>{user.numCubiculo}</td>
+                  <td>{user.numCarnet}</td>
+                  <td>{user.fechaReserv}</td>
+				  <td>{user.horaInicio}</td>
+				  <td>{user.horaFinal}</td>
+				  <td>{user.numReserva}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <br></br>
+        <button onClick={() => navigate('/menuAdmin')} className="est-chooseOption">
+          Regresar
+        </button>
+      </div>
+      <TopBar />
+    </>
+  );
 };
 
 export default ReportAdmin;
