61dc69ec6f6a9a63094a3b2904db0f66c795bd98
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-04-13 23:08:45 -0600

    add images on pdf

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import LogoTec from "../img/logoTEC.svg";
+import Logo from "../img/logoTEC.png";
 import { useNavigate } from 'react-router-dom';
 
 const TopBar = () => {
 	const { jsPDF } = require("jspdf");
 	const doc = new jsPDF();
-	doc.text("Hola mundo", 10, 10);
-
-	//doc.save("prueba.pdf");
+	// doc.text("Hola mundo", 10, 10);
+	doc.addImage(Logo, "PNG", 10, 10, 50, 50);
+	// doc.save("prueba.pdf");
 	const navigate = useNavigate();
 	return (
 		<div id="top-bar">
