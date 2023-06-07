2757d4d850adfee4b5f5a6e8ce65f0a4fa031ebf
Axel Chaves <axelchaves.r@estudiantec.cr>
2023-06-07 00:07:02 -0600

    expo corriendo

---
+++ bibliotec/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+export default function App() {
+  return (
+    <>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+    </View>
+    <WebView source={{ uri: 'https://bibliotecapp.azurewebsites.net/' }} style={{ flex: 1 }} />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 45,
+    backgroundColor: "black",
+  },
+});
